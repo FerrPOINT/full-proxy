733e764248a93bd767ec338b84821b1460d6a8e8
azhukov <azhukov@wm-team.ru>
2025-08-05 14:55:58 +0700

    Add comprehensive JavaScript/TypeScript support to body_filter.lua - handle all JS/TS content types and URL patterns

--- lua/body_filter.lua
+++ lua/body_filter.lua
@@ -36,6 +36,17 @@ local URL_PATTERNS = {
     {pattern = '"domain":%s*"' .. target_escaped .. '"', replacement = '"domain": "' .. proxy_domain .. '"'},
     {pattern = '"url":%s*"https://' .. target_escaped, replacement = '"url": "https://' .. proxy_domain},
     {pattern = '"origin":%s*"https://' .. target_escaped, replacement = '"origin": "https://' .. proxy_domain .. '"'},
+    {pattern = '"baseUrl":%s*"https://' .. target_escaped, replacement = '"baseUrl": "https://' .. proxy_domain},
+    {pattern = '"apiUrl":%s*"https://' .. target_escaped, replacement = '"apiUrl": "https://' .. proxy_domain},
+    {pattern = '"endpoint":%s*"https://' .. target_escaped, replacement = '"endpoint": "https://' .. proxy_domain},
+    
+    -- JavaScript/TypeScript specific patterns
+    {pattern = "baseURL%s*=%s*['\"]https://" .. target_escaped, replacement = "baseURL = '" .. proxy_domain},
+    {pattern = "apiURL%s*=%s*['\"]https://" .. target_escaped, replacement = "apiURL = '" .. proxy_domain},
+    {pattern = "endpoint%s*=%s*['\"]https://" .. target_escaped, replacement = "endpoint = '" .. proxy_domain},
+    {pattern = "fetch%s*%(%s*['\"]https://" .. target_escaped, replacement = "fetch('https://" .. proxy_domain},
+    {pattern = "axios%.get%s*%(%s*['\"]https://" .. target_escaped, replacement = "axios.get('https://" .. proxy_domain},
+    {pattern = "window%.location%s*=%s*['\"]https://" .. target_escaped, replacement = "window.location = 'https://" .. proxy_domain},
     
     -- Bare domain names (in quotes or as values)
     {pattern = '"' .. target_escaped .. '"', replacement = '"' .. proxy_domain .. '"'},
@@ -51,11 +62,26 @@ local TEXT_CONTENT_TYPES = {
     ["application/json"] = true,
     ["application/javascript"] = true,
     ["text/javascript"] = true,
+    ["application/typescript"] = true,
+    ["text/typescript"] = true,
     ["application/xml"] = true,
     ["text/xml"] = true,
     ["application/xhtml"] = true,
     ["text/html"] = true,
     ["text/css"] = true,
+    ["application/ecmascript"] = true,
+    ["text/ecmascript"] = true,
+    ["application/x-javascript"] = true,
+    ["text/x-javascript"] = true,
+    ["application/x-js"] = true,
+    ["text/x-js"] = true,
+    ["application/ld+json"] = true,
+    ["text/plain"] = true,
+    ["application/x-yaml"] = true,
+    ["text/yaml"] = true,
+    ["application/xml+rss"] = true,
+    ["application/atom+xml"] = true,
+    ["application/rss+xml"] = true,
 }
 
 -- Optimized URL replacement function
@@ -89,8 +115,20 @@ local function should_process_content_type(content_type)
         return false
     end
     
+    -- Check exact content type matches
     for pattern, _ in pairs(TEXT_CONTENT_TYPES) do
         if string.find(content_type, pattern, 1, true) then
+            return true
+        end
+    end
+    
+    -- Additional check for common file extensions in content-type
+    local extensions_to_process = {
+        "js", "ts", "jsx", "tsx", "json", "xml", "html", "htm", "css", "txt", "yaml", "yml"
+    }
+    
+    for _, ext in ipairs(extensions_to_process) do
+        if string.find(content_type, ext, 1, true) then
             return true
         end
     end
